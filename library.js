f787b6af904e7a0e9ffb9e77d74dc0a67bea9171
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-26 05:19:17 +1200

    commit

--- library.js
+++ library.js
@@ -12,7 +12,10 @@ function getCards() {
 }
 
 function searchCats() {
-    console.log(document.getElementsByClassName("search-form").value)
+    let searchQuery=document.getElementById("search").value
+    let filteredCats = cats.filter(cat=>cat.name===searchQuery)
+    populateCards(filteredCats)
+    //console.log(filteredCats);
 }
 
     getCards();
@@ -23,7 +26,7 @@ function searchCats() {
             html += `
                 <div class="col">
                     <div class="card h-100">
-                    <a href="./pages/details.html?id=${products[i].reference_image_id}"><img src="https://cdn2.thecatapi.com/images/${products[i].reference_image_id}.jpg" class="card-img-top imgCards" height="275"></a>
+                    <a href="./pages/details.html?id=${products[i].reference_image_id}"><img src="https://cdn2.thecatapi.com/images/${products[i].reference_image_id}.jpg" class="card-img-top imgCards" height="300"></a>
                         <div class="card-body">
                             <h5 class="card-title">${products[i].name}</h5>
                             <p class="card-text">${products[i].description}</p>
