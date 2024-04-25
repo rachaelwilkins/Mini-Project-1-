5b317001ff446bd31ba922993b8e20b22ce01ee3
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-25 20:05:41 +1200

    commit

--- library.js
+++ library.js
@@ -1,27 +1,38 @@
 let numProducts = 70;
 let products = [];
+let cats;
 
 function getCards() {
     fetch(`https://api.thecatapi.com/v1/breeds`)
         .then(res => res.json())
         .then(json => {
-            products = json;
-            let html = ``;
-            for (let i = 0; i < numProducts && i < products.length; i++) {
-                html += `
-                    <div class="col">
-                        <div class="card h-100">
-                        <a href="./pages/details.html?id=${products[i].reference_image_id}"><img src="${products[i].reference_image_id}" class="card-img-top imgCards"></a>
-                            <div class="card-body">
-                                <h5 class="card-title">${products[i].name}</h5>
-                                <p class="card-text">${products[i].description}</p>
-                                <p class="card-text">Known for being: ${products[i].temperament}.</p>
-                            </div>
-                        </div>
-                    </div>
-                `;
-            }
-            document.getElementById('cards').innerHTML = html;
+        cats=json;    
+        populateCards(json);
         });
 }
+
+function searchCats() {
+    console.log(document.getElementsByClassName("search-form").value)
+}
+
     getCards();
+    function populateCards(JSON) {
+        products = JSON;
+        let html = ``;
+        for (let i = 0; i < numProducts && i < products.length; i++) {
+            html += `
+                <div class="col">
+                    <div class="card h-100">
+                    <a href="./pages/details.html?id=${products[i].reference_image_id}"><img src="https://cdn2.thecatapi.com/images/${products[i].reference_image_id}.jpg" class="card-img-top imgCards" height="275"></a>
+                        <div class="card-body">
+                            <h5 class="card-title">${products[i].name}</h5>
+                            <p class="card-text">${products[i].description}</p>
+                            <p class="card-text">Known for being: ${products[i].temperament}.</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+        document.getElementById('cards').innerHTML = html;
+    }
+
